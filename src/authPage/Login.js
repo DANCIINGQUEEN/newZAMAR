5fc6d93275953e8aabe5be7c84e496f6980ece1d
park <mcpc0816@naver.com>
2023-01-09 00:25:01 +0900

    login sing up component merge

--- src/authPage/Login.js
+++ src/authPage/Login.js
@@ -1,13 +1,14 @@
 import React, {useState} from 'react';
-import './login.css'
+// import './login.css'
 import {useNavigate} from "react-router-dom";
 import {useDispatch} from "react-redux";
 import axios from "axios";
 import {login} from "../state";
-import {useTheme, Box} from "@mui/material";
+import {useTheme, Box, TextField, Button} from "@mui/material";
 import {tokens} from "../theme";
 import Header from "../components/Header";
-
+import {Formik} from "formik";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 
 function Login(props) {
@@ -22,6 +23,12 @@ function Login(props) {
 
     const dispatch = useDispatch();
     const navigate = useNavigate();
+
+    const isNonMobile = useMediaQuery("(min-width:600px)");
+
+    const handleFormSubmit = (values) => {
+        console.log(values);
+    };
 
     const setLogin = (e) => {
         e.preventDefault();
@@ -58,38 +65,204 @@ function Login(props) {
 
 
     return (
+        // <Box m="20px">
+        //     <Box display="flex" justifyContent="space-between" alignItems="center">
+        //
+        //         <div className="container">
+        //             <div className="login-container">
+        //                 <Header title="SIGN IN" subtitle="Log in and use the service"/>
+        //                 {/*<input id="item-1" type="radio" name="item" className="sign-in" checked/>*/}
+        //                 {/*<label htmlFor="item-1" className="item">Sign In</label>*/}
+        //                 <div className="login-form">
+        //                     <form onSubmit={setLogin}>
+        //
+        //                         <div className="sign-in-htm">
+        //                             <div className="group">
+        //                                 <input placeholder="email" id="user" type="email" className="input"
+        //                                        value={email}
+        //                                        onChange={(e) => setEmail(e.target.value)}/>
+        //                             </div>
+        //                             <div className="group">
+        //                                 <input placeholder="Password" id="pass" type="password" className="input"
+        //                                        data-type="password" value={password}
+        //                                        onChange={(e) => setPassword(e.target.value)}/>
+        //                             </div>
+        //
+        //                             <div className="group">
+        //                                 <input type="submit" className="button" value="Sign In"/>
+        //                             </div>
+        //                             <div className="hr"></div>
+        //                             <div className="footer">
+        //                                 <a href="/signUp">Create Account</a>
+        //                             </div>
+        //                         </div>
+        //                     </form>
+        //                 </div>
+        //             </div>
+        //         </div>
+        //     <div className="container">
+        //         <div className="login-container">
+        //             <Header title="SIGN UP" subtitle="Log in and use the service"/>
+        //             {/*<input id="item-1" type="radio" name="item" className="sign-in" checked/>*/}
+        //             {/*<label htmlFor="item-1" className="item">Sign In</label>*/}
+        //             <div className="login-form">
+        //                 <form onSubmit={setLogin}>
+        //
+        //                     <div className="sign-in-htm">
+        //                         <div className="group">
+        //                             <input placeholder="email" id="user" type="email" className="input"
+        //                                    value={email}
+        //                                    onChange={(e) => setEmail(e.target.value)}/>
+        //                         </div>
+        //                         <div className="group">
+        //                             <input placeholder="Password" id="pass" type="password" className="input"
+        //                                    data-type="password" value={password}
+        //                                    onChange={(e) => setPassword(e.target.value)}/>
+        //                         </div>
+        //
+        //                         <div className="group">
+        //                             <input type="submit" className="button" value="Sign In"/>
+        //                         </div>
+        //                         <div className="hr"></div>
+        //                         <div className="footer">
+        //                             <a href="/signUp">Create Account</a>
+        //                         </div>
+        //                     </div>
+        //                 </form>
+        //             </div>
+        //         </div>
+        //     </div>
+        //     </Box>
+        //
+        //
+        // </Box>
         <Box m="20px">
-            <div className="container">
-                <div className="login-container">
-            <Header title="SIGN IN" subtitle="Log in and use the service" />
-                    {/*<input id="item-1" type="radio" name="item" className="sign-in" checked/>*/}
-                    {/*<label htmlFor="item-1" className="item">Sign In</label>*/}
-                    <div className="login-form">
-                        <form onSubmit={setLogin}>
+            {/*<Box display="flex" justifyContent="space-between" alignItems="center">*/}
 
-                            <div className="sign-in-htm">
-                                <div className="group">
-                                    <input placeholder="email" id="user" type="email" className="input" value={email}
-                                           onChange={(e) => setEmail(e.target.value)}/>
-                                </div>
-                                <div className="group">
-                                    <input placeholder="Password" id="pass" type="password" className="input"
-                                           data-type="password" value={password}
-                                           onChange={(e) => setPassword(e.target.value)}/>
-                                </div>
+            <Box
+                display="grid"
+                gridTemplateColumns="repeat(7, 1fr)"
+                gridAutoRows="400px"
+                gap="100px"
+                border="1px solid grey"
+            >
+                <Box
+                    gridColumn="span 3"
+                    backgroundColor={colors.primary[400]}
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                    border="1px solid grey"
 
-                                <div className="group">
-                                    <input type="submit" className="button" value="Sign In"/>
-                                </div>
-                                <div className="hr"></div>
-                                <div className="footer">
-                                    <a href="/signUp">Create Account</a>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
+                >
+                    <form onSubmit={setLogin}>
+                        <Header title="Log In" subtitle="Log in and Use the service"/>
+                        <TextField
+                            fullWidth
+                            variant="filled"
+                            type="text"
+                            label="email"
+                            // onBlur={handleBlur}
+                            // onChange={handleChange}
+                            // value={values.firstName}
+                            name="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            // error={!!touched.firstName && !!errors.firstName}
+                            // helperText={touched.firstName && errors.firstName}
+                            // sx={{gridColumn: "span 2"}}
+                        />
+                        <TextField
+                            fullWidth
+                            variant="filled"
+                            type="password"
+                            label="password"
+                            // onBlur={handleBlur}
+                            // onChange={handleChange}
+                            // value={values.lastName}
+                            name="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            // error={!!touched.lastName && !!errors.lastName}
+                            // helperText={touched.lastName && errors.lastName}
+                            // sx={{gridColumn: "span 2"}}
+                        />
+                        <Box display="flex" justifyContent="end" mt="20px">
+                            <Button type="submit" color="secondary" variant="contained">
+                                Log In
+                            </Button>
+                        </Box>
+                    </form>
+                </Box>
+
+                <Box
+                    gridColumn="span 3"
+                    backgroundColor={colors.primary[400]}
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                    border="1px solid grey"
+
+                >
+                    {/*Sign Up*/}
+                    <form onSubmit={setLogin}>
+                        <Header title="Sign Up" subtitle="Create a New User Profile"/>
+                        <TextField
+                            fullWidth
+                            variant="filled"
+                            type="name"
+                            label="name"
+                            // onBlur={handleBlur}
+                            // onChange={handleChange}
+                            // value={values.lastName}
+                            name="text"
+                            // value={name}
+                            // onChange={(e) => setPassword(e.target.value)}
+                            // error={!!touched.lastName && !!errors.lastName}
+                            // helperText={touched.lastName && errors.lastName}
+                            sx={{gridColumn: "span 2"}}
+                        />
+                        <TextField
+                            fullWidth
+                            variant="filled"
+                            type="text"
+                            label="email"
+                            // onBlur={handleBlur}
+                            // onChange={handleChange}
+                            // value={values.firstName}
+                            name="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            // error={!!touched.firstName && !!errors.firstName}
+                            // helperText={touched.firstName && errors.firstName}
+                            sx={{gridColumn: "span 2"}}
+                        />
+                        <TextField
+                            fullWidth
+                            variant="filled"
+                            type="password"
+                            label="password"
+                            // onBlur={handleBlur}
+                            // onChange={handleChange}
+                            // value={values.lastName}
+                            name="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            // error={!!touched.lastName && !!errors.lastName}
+                            // helperText={touched.lastName && errors.lastName}
+                            sx={{gridColumn: "span 2"}}
+                        />
+
+                        <Box display="flex" justifyContent="end" mt="20px">
+                            <Button type="submit" color="secondary" variant="contained">
+                                Sign Up
+                            </Button>
+                        </Box>
+                    </form>
+                </Box>
+            </Box>
+
+            {/*</Box>*/}
         </Box>
     );
 }
