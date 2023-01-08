450646f34a6a1616e9473431cd1bf5fe18f48090
park <mcpc0816@naver.com>
2023-01-08 12:51:46 +0900

    nivo delete

--- src/App.js
+++ src/App.js
@@ -43,16 +43,16 @@ function App() {
                                 <Route path="/" element={isAuth ? <Dashboard/> : <Login/>}/>
                                 <Route path="/team" element={<Team/>}/>
                                 <Route path="/signUp" element={<SignUp/>}/>
-                                <Route path="/contacts" element={<Contacts/>}/>
+                                {/*<Route path="/contacts" element={<Contacts/>}/>*/}
                                 <Route path="/sheets" element={<Sheet/>}/>
                                 <Route path="/invoices" element={<Invoices/>}/>
                                 <Route path="/form" element={<Form/>}/>
-                                <Route path="/bar" element={<Bar/>}/>
-                                <Route path="/pie" element={<Pie/>}/>
-                                <Route path="/line" element={<Line/>}/>
+                                {/*<Route path="/bar" element={<Bar/>}/>*/}
+                                {/*<Route path="/pie" element={<Pie/>}/>*/}
+                                {/*<Route path="/line" element={<Line/>}/>*/}
                                 <Route path="/faq" element={<FAQ/>}/>
                                 <Route path="/calendar" element={<Calendar/>}/>
-                                <Route path="/geography" element={<Geography/>}/>
+                                {/*<Route path="/geography" element={<Geography/>}/>*/}
                             </Routes>
                         </main>
                     </div>
